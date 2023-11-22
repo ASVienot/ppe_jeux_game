56f6f65acca2c516e3a57160a2a1d08a499643cb
PDegez <pauline.degez@gmail.com>
2023-11-22 11:06:13 +0100

    version annotée de aspiration.sh

--- programmes/aspiration.sh
+++ programmes/aspiration.sh
@@ -1,21 +1,26 @@
 #!usr/bin/bash
+#programme qui prends 2 arguments :
+#1 = une liste d'urls en .txt
+#2 = un nom de langue qu'il va renvoyer comme titre des outputs (par exemple FR, EN ou CH)
 
 URLS=$1
 langue=$2
 
+#vérification nombre d'arguments = 2
 if [[ $# -ne 2 ]];
 then
 	echo "On veut exactement deux arguments au script : une liste d'url au format .txt et un nom de langue"
 	exit
 fi
 
+#vérification argument1 est un fichier (pas un dossier)
 if [ ! -f $URLS ]
 then
 	echo "On attend un fichier, pas un dossier"
 	exit
 fi
 
-lineno=1
+#Boucle d'extraction : pour chaque ligne (=chaque url de la liste) extrait le contenu brut et l'enregistre dans un fichier .txt dans le dossier ./URLs
 while read -r URL
 do
 	data_brut=$(curl $URL)
