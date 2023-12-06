953a4cefd7a0e713996cec643f113afdf06430b0
PDegez <pauline.degez@gmail.com>
2023-12-06 09:44:11 +0100

    fichiers trameurs version propre

--- programmes/make_itrameur_corpus.sh
+++ programmes/make_itrameur_corpus.sh
@@ -4,6 +4,7 @@ DOSSIER=$1
 LANGUE=$2
 
 LISTE_FICHIER=$(ls $DOSSIER)
+BASED=$(basename ${DOSSIER})
 
 if [ ! -d $DOSSIER ]
 then
@@ -17,21 +18,23 @@ then
 	exit
 fi
 
+echo -e "<lang=\"$LANGUE\">" > "../itrameur/${BASED}-${LANGUE}.txt"
+
 lineno=1
 for FICHIER in $LISTE_FICHIER
 do
 	BASEF=$(basename ${FICHIER} .txt)
-	BASED=$(basename ${DOSSIER})
 
 	if [[ $BASEF =~ $LANGUE\- ]]
 	then
 		contenu=$(cat $DOSSIER/$FICHIER | sed -e 's/&/&amp/g ; s/>/&gt/g ; s/</&lt/g')
-		echo -e "<lang=\"$LANGUE\">\n<page=\"${LANGUE}-${lineno}\">\n<text>\n${contenu}</text>\n</page> §\n" >> "../itrameur/${BASED}-${LANGUE}.txt"
+		echo -e "\n<page=\"${LANGUE}-${lineno}\">\n<text>\n${contenu}\n</text>\n</page> §\n" >> "../itrameur/${BASED}-${LANGUE}.txt"
 
 	lineno=$(expr $lineno + 1)
 	fi
 
 done
 
+echo -e "</lang>" >> "../itrameur/${BASED}-${LANGUE}.txt"
 echo "fin du programme"
 
